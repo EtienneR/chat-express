4afc398f517e710ba3241b38e94bfd44813084ad
EtienneR <etienne.rouzeaud@free.fr>
2015-05-26 16:41:50 +0200

    Modification CSS

--- public/js/helper.js
+++ public/js/helper.js
@@ -1,6 +1,5 @@
 function getNbUsers(nbUsers) {
   $('#left h2').hide();
-
   if (nbUsers == 1) {
     $('#users').before('<h2>' + nbUsers + ' utilisateur connecté</h2>');
   } else {
@@ -9,18 +8,22 @@ function getNbUsers(nbUsers) {
 }
 
 function getUsers(users) {
-  $('#users li').hide();
+  //$('#users li').hide();
   for (i = 0; i < users.length; i++) {
-    $('#users').prepend('<li class="' + users[i] + '"><img src="http://api.adorable.io/avatar/32/' + users[i] + '" alt=""/><span>' + users[i] + '</span></li>');
+    $('span.' + users[i] + ' > span.status').removeClass('offline');
+    $('span.' + users[i] + ' > span.status').addClass('online');
+    $('span.' + users[i] + ' > span.more').hide();
+    $('span.' + users[i]).append('<span class="more">online</span>');
+    //$('#users').prepend('<li class="' + users[i] + '"><img src="http://api.adorable.io/avatar/32/' + users[i] + '" alt=""/><span>' + users[i] + '</span></li>');
   }
-  $('#users li.' + currentLogin).hide();
+  //$('#users li.' + currentLogin).hide();
 }
 
 function insereMessage(login, message) {
   if (currentLogin == login) {
-    $('#messages').append('<li class="current"><p>' + message + '</p><div id="message-info"><span>[' + date() + ']</span><img src="http://api.adorable.io/avatar/32/' + login + '" alt="" /></div></li>');
+    $('#messages').append('<li class="me"><span class="about"><span class="more">' + date() + '</span>, <b>' + login + '</b></span><div class="arrow-up right"></div><p>' + message + '</p></li>');
   } else {
-    $('#messages').append('<li><p>' + message + '</p><div id="message-info"><span>' + login + ' [' + date() + ']</span><img src="http://api.adorable.io/avatar/32/' + login + '" alt="" /></div></li>');
+    $('#messages').append('<li><span class="about"><b>' + login + '</b>, <span class="more">' + date() + '</span></span><div class="arrow-up left"></div><p>' + message + '</p></li>');
   }
 }
 
